e7a9de9424791ced6416a99220bbaa7f13583029
Mark McDonnell <Integralist@users.noreply.github.com>
2021-11-26 15:21:24 +0000

    Add source code from Fiddle

--- src/index.js
+++ src/index.js
@@ -1,68 +1,33 @@
-//! Default Compute@Edge template program.
-import welcomePage from "./welcome-to-compute@edge.html";
+/// <reference types="@fastly/js-compute" />
 
-// The entry point for your application.
-//
-// Use this fetch event listener to define your main request handling logic. It could be
-// used to route based on the request properties (such as method or path), send
-// the request to a backend, make completely new requests, and/or generate
-// synthetic responses.
+addEventListener("fetch", async (event) => {
 
-addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
+  const req = event.request;
 
-async function handleRequest(event) {
-  // Get the client request.
-  let req = event.request;
-
-  // Filter requests that have unexpected methods.
-  if (!["HEAD", "GET"].includes(req.method)) {
-    return new Response("This method is not allowed", {
-      status: 405,
-    });
-  }
-
-  let url = new URL(req.url);
-
-  // If request is to the `/` path...
-  if (url.pathname == "/") {
-    // Below are some common patterns for Compute@Edge services using JavaScript.
-    // Head to https://developer.fastly.com/learning/compute/javascript/ to discover more.
-
-    // Create a new request.
-    // let bereq = new Request("http://example.com");
-
-    // Add request headers.
-    // req.headers.set("X-Custom-Header", "Welcome to Compute@Edge!");
-    // req.headers.set(
-    //   "X-Another-Custom-Header",
-    //   "Recommended reading: https://developer.fastly.com/learning/compute"
-    // );
-
-    // Create a cache override.
-    // let cacheOverride = new CacheOverride("override", { ttl: 60 });
-
-    // Forward the request to a backend.
-    // let beresp = await fetch(req, {
-    //   backend: "backend_name",
-    //   cacheOverride,
-    // });
-
-    // Remove response headers.
-    // beresp.headers.delete("X-Another-Custom-Header");
-
-    // Log to a Fastly endpoint.
-    // const logger = fastly.getLogger("my_endpoint");
-    // logger.log("Hello from the edge!");
-
-    // Send a default synthetic response.
-    return new Response(welcomePage, {
-      status: 200,
-      headers: new Headers({ "Content-Type": "text/html; charset=utf-8" }),
-    });
-  }
-
-  // Catch all other requests and return a 404.
-  return new Response("The page you requested could not be found", {
-    status: 404,
+  let backendResponse = await fetch(req, {
+    backend: "origin_0"
   });
-}
+
+  if (backendResponse.status == 200) {
+    // Parse the URL so we can determine the path.
+    let url = new URL(req.url);
+
+    // If the request is to a product endpoint...
+    if (url.pathname.startsWith('/products/')) {
+      // Parse the JSON response from the backend.
+      let data = await backendResponse.json();
+
+      // Add a new field to the parsed data.
+      data["new_field"] = "data injected at the edge";
+
+      // Construct a new response using the new data but original status and headers.
+      backendResponse = new Response(JSON.stringify(data), {
+        status: backendResponse.status,
+        headers: backendResponse.headers
+      });
+    }
+  }
+
+  // Send the backend response to the client.
+  event.respondWith(backendResponse);
+});
